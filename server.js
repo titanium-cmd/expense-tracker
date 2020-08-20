fc3178ad208bad4641ba67fe4c15d4ec5ccfc294
titanium <nkansahisaac41@gmail.com>
2020-08-20 03:03:40 +0000

    dont with backend

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ dotenv = require('dotenv'),
 colors = require('colors'),
 app = express();
 
+dotenv.config({ path: './config/config.env' });
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(morgan('dev'));
@@ -17,7 +18,6 @@ if(process.env.NODE_ENV === 'production'){
     app.get('*', (req, res)=>res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
 
-dotenv.config({ path: './config/config.env' });
 connectDB();
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>console.log(`Server running on ${process.env.NODE_ENV} mode port ${PORT}`.yellow.bold));
