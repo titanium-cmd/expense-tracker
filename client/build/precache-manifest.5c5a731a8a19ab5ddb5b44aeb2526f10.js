60bb6ec8043b882274f64d2899399813525c5f24
titanium <nkansahisaac41@gmail.com>
2020-08-20 03:11:39 +0000

    dont with backend

---
+++ client/build/precache-manifest.5c5a731a8a19ab5ddb5b44aeb2526f10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14befbb5953756568611c16664863656",
+    "url": "/index.html"
+  },
+  {
+    "revision": "295741d333321d6e90d3",
+    "url": "/static/css/main.ba037b4b.chunk.css"
+  },
+  {
+    "revision": "2f53f17abb3299f1bfa3",
+    "url": "/static/js/2.8d0a1112.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8d0a1112.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "295741d333321d6e90d3",
+    "url": "/static/js/main.e2733448.chunk.js"
+  },
+  {
+    "revision": "36a1a4fcc23d397bc2ef",
+    "url": "/static/js/runtime-main.152a6c68.js"
+  }
+]);
